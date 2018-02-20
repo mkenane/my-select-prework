fd9cdf4ede9b47f970d71253d57f2d81f158039a
Mor Kenane <morkenan@hotmail.com>
2018-02-20 20:06:53 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
   newcollection = []
 i = 0 
 while i < collection.length 
-if yield(collection[i] == true 
-  newcollection.push()
+if yield(collection[i]) == true 
+  newcollection.push(collection[i])
 i += 1 
 end 
 newcollection
